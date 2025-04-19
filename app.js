63f38e5f2150a07424c3be3cf98595c9de32c220
Akshay Darekar <akshaydarekar5252@gmail.com>
2025-04-19 19:56:50 +0530

    phase a

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const Listing = require('./models/listing.js');
 const path = require('path');
+const methodOverride = require('method-override');
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -22,6 +23,8 @@ async function main() {
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use (express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
 
 
 app.get("/",(req,res)=>{
@@ -34,12 +37,69 @@ app.get("/listings",async (req,res)=>{
        res.render("listings/index.ejs",{allListings}); 
 });
 
+//New Route 
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
+
 //show route 
 app.get("/listings/:id",async (req,res)=>{
     let  {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
 });
+
+//Create route
+
+app.post("/listings",async (req,res)=>{
+    // const {title,description,image,price,location,country} = req.body;
+    // const newListing = new Listing({
+    //     title,
+    //     description,
+    //     price,
+    //     location,
+    //     country
+    // });
+    // await newListing.save();
+    // res.redirect("/listings");
+
+
+    //or method 
+
+   
+    const newListing = new Listing(req.body.listings);
+    await newListing.save();
+    res.redirect("/listings");
+    
+
+});
+
+//Edit route 
+
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+
+// Update route
+
+app.put("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listings});
+   res.redirect(`/listings/${id}`);
+    
+}
+);
+
+//Delete route
+app.delete("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+     let deletedListing = await Listing.findByIdAndDelete(id);
+     console.log(deletedListing);
+    res.redirect("/listings");
+});
+
 
 // app.get("/testListing", async (req,res)=>{
 //     let sampleListing = new Listing({
